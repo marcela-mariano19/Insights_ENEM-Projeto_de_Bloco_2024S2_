357910069b3ff4469aa0b78236259a77c096e99a
Marcela Mariano <77173412+marcela-mariano19@users.noreply.github.com>
2024-10-23 23:39:20 -0300

    v2 - Projeto de Bloco

--- my-app/app/app.py
+++ my-app/app/app.py
@@ -15,26 +15,49 @@ st.set_page_config(
 
 def dash_Uf(ufs_dict):
     df_ufs = pd.DataFrame(list(ufs_dict.items()),columns = ['UF','Total de Provas Aplicadas'])
-    fig, ax = plt.subplots( figsize=(7,3))
-    ax.bar(df_ufs['UF'], df_ufs['Total de Provas Aplicadas'])
-    ax.tick_params(axis='x', rotation=45)
-    #ax.legend()
-    return fig
+    df_ufs.set_index('UF', inplace=True)
+    
+    return st.bar_chart(df_ufs,)
 
+def dash_Uf_MH(df_uf_region_full):
 
+    return st.bar_chart(df_uf_region_full,x='UF',y=['Total Homens Inscritos','Total Mulheres Inscritas','Total Homens Presentes','Total Mulheres Presentes'],stack=False)
 
+def dash_Uf_selected_MH(df_send,seletions):
+    df_send = df_send[df_send['UF'].isin(seletions)]
+
+    return st.bar_chart(df_send,x='UF',y=['Total Homens Inscritos','Total Mulheres Inscritas','Total Homens Presentes','Total Mulheres Presentes'],stack=False)
+
+@st.cache_data
+def load_data():
+    df  = pd.read_parquet("my-app/data/analysis/df_metrics_inscription.parquet")
+    df_uf_region_full = pd.read_parquet("my-app/data/analysis/df_uf_region_full.parquet")
+
+    return df,df_uf_region_full
 
 
 def main ():
-    menu = ['Home', 'Nevegando nos Dados', 'Criadora','TP1']
+    menu = ['Home','Dados Gerais 2023', ' Visão Inscritos','Visão Concluintes','Visão Desempenho','Fatores de Contexto','Personalizado', 'Criadora','TP1']
 
     st.title("Insights ENEM")
     choice = st.sidebar.selectbox('Menu', menu)
 
+    df, df_uf_region_full  = load_data()
+
     if choice == 'Home':
         st.subheader('O que é o ENEM Insights?')
         st.write("Hoje, a maior parte das pessoas que trabalham com educação no Brasil não conseguem acessar informações sobre o ENEM de forma fácil e rápida. Visando curar essa dor surgiu o Enem Insights, uma aplicação que permite a qualquer pessoa acessar informações sobre o ENEM de forma fácil e prática.")
-        df  = pd.read_parquet("my-app/data/analysis/df_metrics_inscription.parquet")
+
+        st.subheader('Por que o ENEM Insights foi criado?')
+        st.markdown("O [Instituto INFNET](https://www.infnet.edu.br/infnet/home/) propôs que os alunos do curso de Ciência de Dados criassem um projeto de bloco ligado aos Objetivos de Desenvolvimento Sustentável no Brasil, então a aluna Marcela Mariano propôs o projeto Enem Insights, que une seu amor pela educação e pela análise de dados.") 
+
+        st.subheader("Esta aplicação tem ligação com qual Objetivos de Desenvolvimento Sustentável?")
+        st.markdown("Este projeto está ligado ao ODS 4 - Educação de Qualidade, pois visa facilitar o acesso a informações sobre o ENEM, que é uma das principais formas de acesso ao ensino superior no Brasil. Esses dados ajudam a entender melhor o cenário educacional do país e a identificar possíveis melhorias. Quer saber mais sobre as metas do ODS 4? [Clique aqui](https://brasil.un.org/pt-br/sdgs/4)")
+    
+        st.subheader("De onde vêm os dados?")
+        st.markdown("Os dados utilizados nesta aplicação são públicos e foram disponibilizados pelo [INEP](https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados/enem). Legal, né? O governo brasileiro oferece esses e outros dados devido a lei de Dados Abertos. ")
+        
+    elif choice == 'Dados Gerais 2023':
 
         st.subheader('Métricas de Inscrição:')
         col1,col2,col3 = st.columns(3)
@@ -51,10 +74,53 @@ def main ():
             col6.metric(label="Total de Homens Presentes", value=df['Total de Homens inscritos'].apply(lambda x: f"{x:,.0f}".replace(',', '.')).iloc[0])
 
         st.subheader('Provas Aplicadas por UF')
+        
+        #Total de Provas Aplicadas por UF
+        dash_Uf(df['Total de Provas Aplicadas por UF'][0])
 
-        st.pyplot(dash_Uf(df['Total de Provas Aplicadas por UF'][0]))
+        #Total de Mulheres e Homens iscritos vs Presentes por UF
+        st.subheader('Total de Mulheres e Homens Inscritos vs Presentes por UF')
+        dash_Uf_MH(df_uf_region_full)
+
+        if 'ufs_selected' not in st.session_state:
+            st.session_state['ufs_selected'] = []
+
+        st.write("Quer exibir somente alguns UF's? Selecione abaixo:")
+        ufs_selected = st.multiselect('UFs', df_uf_region_full['UF'].sort_values(), default=st.session_state['ufs_selected'])
+
+        st.session_state['ufs_selected'] = ufs_selected
+
+        if ufs_selected:
+            dash_Uf_selected_MH(df_uf_region_full,ufs_selected)
+
+
+        st.subheader('Dados Gerais para Download:')
+
+        st.dataframe(df_uf_region_full)
         
 
+
+        
+        
+    elif choice == ' Visão Inscritos':
+        st.write('EM DESENVOLVIMENTO')
+
+    elif choice == 'Visão Concluintes':
+        st.write('EM DESENVOLVIMENTO')
+    
+    elif choice == 'Visão Desempenho':
+        st.write('EM DESENVOLVIMENTO')
+    
+    elif choice == 'Fatores de Contexto':
+        st.write('EM DESENVOLVIMENTO')
+
+    elif choice == 'Personalizado':
+        st.write('EM DESENVOLVIMENTO')
+    
+    elif choice == 'Criadora':
+        st.subheader('Marcela Mariano')
+        st.write('Aluna do curso de Ciência de Dados do Instituto INFNET')
+        st.write('Linkedin: [Marcela Mariano](https://www.linkedin.com/in/marcela-mariano-8a0b6a1b4/)')
 
 
     elif choice == 'TP1':
